48f9360b857796f9090e9621691796a76743d1cc
panboxiaosa <864633261@qq.com>
2016-01-05 19:39:23 +0800

    kmp

--- kmp/kmp.cpp
+++ kmp/kmp.cpp
@@ -5,10 +5,34 @@
 
 using namespace std;
 
-char pattern[10000];
-int next_array[10000];
-char target[1000000];
+char pattern[10001];
+int next_array[10001];
+char target[1000001];
 int len;
+
+void GetNext()
+{
+    int i=0;
+    int j=-1;
+    next_array[i]=j;
+    while(i<len)
+    {
+        if(j==-1||pattern[i]==pattern[j])
+        {
+            i++;
+            j++;
+            if(i==len||pattern[i]!=pattern[j])//不允许出现P[i]==P[next[i]]
+                next_array[i]=j;
+            else
+                next_array[i]=next_array[j];
+        }
+        else
+        {
+            j=next_array[j];
+        }
+    }
+
+}
 
 void getnext_array()
 {
@@ -47,6 +71,32 @@ int findMatch()
         }
     }
     return occur;
+}
+
+int KMPSearch()
+{
+    int Plen = len;
+    int Slen = strlen(target);
+
+    unsigned int i=0;//在S串中的下标
+    unsigned int j=0;//在P串中的下标
+	unsigned int count=0;//匹配串出现的次数
+    while(i<Slen)
+    {
+        if(j==-1||target[i]==pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else  if(j<Plen)
+            j=next_array[j];
+		if(j==Plen)
+		{
+			count++;
+			j=next_array[Plen];
+		}
+    }
+    return count;
 }
 
 int main()
