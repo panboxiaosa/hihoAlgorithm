9cfb5f9e1e6ff4737c9ae6a6ebd4e22d3fb592a6
panboxiaosa <864633261@qq.com>
2016-01-11 20:39:54 +0800

    ehllo

--- interleaving/main.cpp
+++ interleaving/main.cpp
@@ -7,19 +7,27 @@ using namespace std;
 
 const int mod_tar = 1000000007;
 const int dig_num = 10;
+long long dp[20][10][220];
 
-int getCross(string first, string second, int match){
-    int len1 = first.length();
-    int len2 = second.length();
+int getSum(string str)
+{
+    int sum = 0;
+    int hold = -1;
+    for(int i = 0; i < str.length(); i++){
+        hold *= -1;
+        sum += hold * (str[i] - '0');
+    }
+}
 
-    int* digits = new int[len1];
-    for(int i = 0; i < len1; i++)
-        digits[i] = first.at(i) - '0';
+int getCross(string target, int match){
+    int len = target.length();
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j <= target[i] - '0'; j++){
+            for(k = -109; k < 109; k++){
 
-
-
-    TwoDArray smaller(len1, dig_num);
-    delete[] digits;
+            }
+        }
+    }
 
     return 0;
 }
@@ -28,7 +36,7 @@ int getCross(string first, string second, int match){
 int main()
 {
     string first;
-    string second;
+    string second
     int match;
     cin>>first;
     cin>>second;
