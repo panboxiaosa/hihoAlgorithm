5ba59b975543b40bdb1091dbf9eafc577976843b
panboxiaosa <864633261@qq.com>
2016-01-12 21:01:29 +0800

    cross

--- interleaving/main.cpp
+++ interleaving/main.cpp
@@ -1,46 +1,85 @@
 #include <iostream>
-#include <string>
-#include <string.h>
-#include "../util/array2D.hpp"
-
+#include <memory.h>
 using namespace std;
 
-const int mod_tar = 1000000007;
-const int dig_num = 10;
-long long dp[20][10][220];
+long long mod = 1000000007;
+int store[20];
+int nowCal;
+long long bitBase[20];
+int offset = 200;
 
-int getSum(string str)
+struct node {
+    long long s,n;
+};
+node dp[20][400];
+
+
+node dfs(int pos, int target, bool limit)
 {
-    int sum = 0;
-    int hold = -1;
-    for(int i = 0; i < str.length(); i++){
-        hold *= -1;
-        sum += hold * (str[i] - '0');
+    node one;
+    one.s = one.n = 0;
+    if(pos < 0){
+        if(target == offset)
+            one.n = 1;
+        return one;
     }
-}
+    if(!limit && dp[pos][target].n != -1)
+        return dp[pos][target];
+    int start = (pos == nowCal ? 1: 0);
+    int finish = limit ? store[pos]: 9;
+    int sgn = (nowCal - pos) % 2 ? -1: 1;
 
-int getCross(string target, int match){
-    int len = target.length();
-    for(int i = 0; i < len; i++){
-        for(int j = 0; j <= target[i] - '0'; j++){
-            for(k = -109; k < 109; k++){
-
-            }
+    for(int i = start;i <= finish; i++){
+        node tmp = dfs(pos - 1, target - sgn*i, limit&& i == finish);
+        if(tmp.n > 0){
+            one.n += tmp.n;
+            one.s += tmp.s;
+            one.s %= mod;
+            long long added = ((tmp.n %mod)*bitBase[pos] %mod * i)%mod;
+            one.s += added;
+            one.s %= mod;
         }
     }
 
-    return 0;
+    if(!limit)
+        dp[pos][target] = one;
+
+    return one;
+
 }
 
+long long cal(long long num,long long match){
+    int index = 0;
+    while(num > 0){
+        store[index] = num % 10;
+        num /= 10;
+        index++;
+    }
+    long long ans = 0;
+    for(nowCal = 0; nowCal < index; nowCal++){
+        memset(dp, -1, sizeof(dp));
+        ans += dfs(nowCal, match + offset, nowCal == index - 1).s;
+        ans = (ans + mod) % mod;
+
+    }
+    return ans;
+}
 
 int main()
 {
-    string first;
-    string second
-    int match;
-    cin>>first;
-    cin>>second;
-    cin>>match;
-    cout<<getCross(first, second, match)<<endl;
+    bitBase[0] = 1;
+    for(int i = 1 ;i < 19; i++){
+        bitBase[i] = (bitBase[i - 1] * 10) % mod;
+    }
+
+    long long start;
+    long long finish;
+    long long match;
+    cin>>start>>finish>>match;
+
+    long long resus = cal(start, match);
+    long long resuf = cal(finish, match);
+
+    cout<< (resuf - resus + mod) % mod<<endl;
     return 0;
 }
