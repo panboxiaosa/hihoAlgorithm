980873312c489cc17beb442bd75ba6b07d659bdc
panboyuan <panboyuan@baidu.com>
2016-10-15 09:09:34 +0800

    test

--- test/main.cpp
+++ test/main.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
+#include <limits>
+#include <string>
+#include <vector>
 
 using namespace std;
-
-char matrix[200][200];
-
-char temp[200][200];
-
-char target[3][3];
-
-void rotate(int w, int h){
-    for(int i = 0; i < 200; i++)
-        for(int j = 0; j < 200; j++)
-            temp[i][j] = matrix[199 - j][i];
-
-    for(int i = 0; i < 200; i++)
-        for(int j = 0; j < 200; j++)
-            matrix[i][j] = 0;
-
-    for(int i = 0; i < w; i++)
-        for(int j = 0; j < h; j++)
-            matrix[i][j] = temp[i][j + 200 - h];
-}
-
+bool valid[100001];
 
 int main(){
 
-    int m,n;
-    cin>>m>>n;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
-            cin>>matrix[i][j];
+    int a;
+    cin>>a;
 
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-            cin>>target[i][j];
-
-
-    for(int i = 0; i < 4; i++){
-        rotate(m ,n);
-        swap(m, n);
-        for(int y = 1; y < n - 1; y++){
-            for(int x = 1; x < m - 1; x++){
-                bool result = true;
-                for(int i = 0; i < 3; i++)
-                    for(int j = 0; j < 3; j++)
-                        if(matrix[y + i - 1][x + j- 1] != target[i][j])
-                            result = false;
-                if(result)
-                    cout<<x + 1<<" "<<y + 1<<endl;
-            }
+    for (int i = 0 ;i <= 100000; i++){
+        int temp = i;
+        int bin = 0;
+        int ten = 0;
+        while(temp > 0){
+            bin += temp & 1;
+            temp >>= 1;
+        }
+        temp = i;
+        while(temp > 0){
+            temp += temp % 10;
+            temp /= 10;
+        }
+        if (bin == ten){
+            valid[i] = true;
+        }else{
+            valid[i] = false;
         }
     }
-
+    vector<int> result;
+    result[0] = 1;
+    for (int i = 1; i <= 100000; i++)
+    {
+        if (valid[i])
+        {
+            result[i] = result[i-1] + 1;
+        }
+    }
+    int b;
+    for (int i = 0; i < a; i++)
+    {
+        cin>> b;
+        cout<<result[b]<<endl;
+    }
     return 0;
 }
